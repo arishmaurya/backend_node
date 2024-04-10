cab80c5eb876321a7c97e0279dbba2693ffd0ded
Rahul Maurya <arish2411rahul@gmail.com>
2024-04-10 17:30:14 +0530

    day6 done

--- day_6/Backend/index.js
+++ day_6/Backend/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
 let port = 8080;
-
+app.use(express.urlencoded({ extended: true })); // this line help express to understand the data
+app.use(express.json()); // this line help express to understand  Json  data
 app.get("/register", (req, res) => {
-  let { username, Password } = req.query;
-  res.send(`Get Response is => ${username} and password is => ${Password}`);
+  let { username, password } = req.query;
+  res.send(`Get Response is => ${username} and password is => ${password}`);
 });
 
 app.post("/register", (req, res) => {
-  let { username, Password } = req.query;
-  res.send(`Post Response is => ${username} And password is => ${Password}`);
+  //   let { username, Password } = req.query;
+  //   res.send(`Post Response is => ${username} And password is => ${Password}`);
+  res.send(
+    `Username is ${req.body.username} and Password is ${req.body.password}`
+  );
 });
 
 app.listen(port, () => {
